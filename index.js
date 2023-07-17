5ef6b7c62ff9d93edda516b84d32999be166862b
vaishnavin01 <107188328+vaishnavin01@users.noreply.github.com>
2023-07-17 21:16:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function imgSlider(anything){
+    document.querySelector('.smoothy').src=anything;
+ }
+ function changecircle(color){
+     const circle=document.querySelector('.circle');
+     circle.style.background=color;
+ }
+ function thumbbg(color){
+     const thumb=document.querySelector('.thumbnail');
+     thumb.style.background=color;
+ }
+ function changebg(color){
+     const sec=document.querySelector('.section');
+     sec.style.background=color;
+ }
